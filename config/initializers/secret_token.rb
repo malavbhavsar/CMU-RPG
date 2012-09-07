348fc89e229a80733ee3ac65feff0aa6c741e552
malavbhavsar <malav.bhavsar@gmail.com>
2012-09-07 19:25:24 -0400

    Wait a sec!

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-CMURpg::Application.config.secret_token = '7cf7006fc1548c13e74e9a7c1cd86eee5c924e3c545f0210bdb03b2639f520fb4f71fffaa412187d8ee071b4393baefa1a3ea0d4fb11c2013a69936197d8d5f3'
